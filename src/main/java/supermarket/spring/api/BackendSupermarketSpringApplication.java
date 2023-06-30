ac9c9d40d69d24b02340e84beae7999ebd307969
Juan Manuel Quijano Pardo <juanquijano1352@gmail.com>
2023-06-29 22:35:15 -0500

    Initial Config, spring project ( supermarket )

---
+++ src/main/java/supermarket/spring/api/BackendSupermarketSpringApplication.java
@@ -0,0 +1,13 @@
+package supermarket.spring.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSupermarketSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSupermarketSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/supermarket/spring/api/BackendSupermarketSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package supermarket.spring.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSupermarketSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
